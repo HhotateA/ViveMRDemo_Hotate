02339ecd6a67dde92fb06e6aae26bf88f3d29208
hhotatea <hhotatea@gmail.com>
2019-12-04 07:32:32 +0900

    Add file

---
+++ Assets/MRSkybox.shader
@@ -0,0 +1,62 @@
+﻿Shader "MR/Skybox"
+{
+    Properties {
+        _MainTex ("Texture", 2D) = "white" {}
+        _EyeLevel ("EyeLevel",range(-100,100)) = 0.0
+    }
+    SubShader
+    {
+        Tags
+        {
+            "RenderType"="Background"
+            "Queue"="Background"
+            "PreviewType"="SkyBox"
+        }
+
+        Pass
+        {
+            ZWrite Off
+            Cull Off
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_fog
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            float _EyeLevel;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 texcoord : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.texcoord = ComputeGrabScreenPos(o.vertex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                i.texcoord.x += unity_StereoEyeIndex == 0 ? _EyeLevel : -_EyeLevel;
+                fixed4 col = tex2Dproj(_MainTex,i.texcoord);
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/UnityWindowsFileDrag-Drop-master/FileDragAndDrop.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using B83.Win32;
+using VRM;
+using UnityEngine.Playables;
+
+
+public class FileDragAndDrop : MonoBehaviour
+{
+    // important to keep the instance alive while the hook is active.
+    UnityDragAndDropHook hook;
+    [SerializeField] Animator move_root;
+    [SerializeField] Animator vrm_root;
+    [SerializeField] PlayableDirector time_line;
+    void OnEnable ()
+    {
+        // must be created on the main thread to get the right thread id.
+        hook = new UnityDragAndDropHook();
+        hook.InstallHook();
+        hook.OnDroppedFiles += OnFiles;
+        var binding = time_line.playableAsset.outputs.First (c => c.streamName == "MoveRoot");
+        time_line.SetGenericBinding(binding.sourceObject,move_root);
+        SetTimeline();
+    }
+    void OnDisable()
+    {
+        hook.UninstallHook();
+    }
+
+    void OnFiles(List<string> aFiles, POINT aPos)
+    {
+        // do something with the dropped file names. aPos will contain the 
+        // mouse position within the window where the files has been dropped.
+        Debug.Log("Dropped "+aFiles.Count+" files at: " + aPos + "\n"+
+            aFiles.Aggregate((a, b) => a + "\n" + b));
+        
+        string path0 = aFiles[0]; 
+            
+        try{
+            byte[] bytes = System.IO.File.ReadAllBytes(path0);
+            var tempVRM = VRMImporter.LoadFromBytes(bytes);
+            tempVRM.transform.SetParent(move_root.transform);
+            tempVRM.transform.position = vrm_root.transform.position;
+            tempVRM.transform.rotation = vrm_root.transform.rotation;
+            Destroy(vrm_root.gameObject);
+            vrm_root = tempVRM.GetComponent<Animator>() as Animator;
+            SetTimeline();
+        }catch{
+            Debug.Log("MissingRoad");
+            return;
+        }
+    }
+
+    void SetTimeline(){
+        var binding = time_line.playableAsset.outputs.First (c => c.streamName == "VRMRoot");
+        time_line.SetGenericBinding(binding.sourceObject,vrm_root);
+    }
+}
